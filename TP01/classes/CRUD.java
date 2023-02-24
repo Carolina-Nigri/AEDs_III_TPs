1195d67db03f847f6e1d11f203ee1c9258b0eddc
pdMiranda <pedromrodrigues1807@gmail.com>
2023-02-24 15:21:59 -0300

    update CRUD base

--- TP01/classes/CRUD.java
+++ TP01/classes/CRUD.java
@@ -1,7 +1,22 @@
 /** Pacotes **/
 package TP01.classes;
 
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
 /** Classe CRUD **/
 public class CRUD {
-    
+
+    RandomAccessFile arq;
+
+    CRUD() throws FileNotFoundException{
+        this.arq = new RandomAccessFile("Teste" , "rw");
+    }
+    CRUD(String nome) throws FileNotFoundException{
+        this.arq = new RandomAccessFile(nome , "rw");
+    }
+
+    public static void create(Musica obj){
+        
+    }
 }
