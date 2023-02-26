0f77563a971185d386d68f9bb254fda3e078f728
Carolina-Nigri <carolnigri115@gmail.com>
2023-02-26 18:24:11 -0300

    alteracoes na Main (tem erros no CRUD)

--- TP01/TP01.java
+++ TP01/TP01.java
@@ -1,3 +1,4 @@
+/** Pacotes **/
 package TP01;
 import TP01.classes.CRUD;
 import TP01.classes.Musica;
@@ -5,125 +6,192 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Date;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+/* Classe TP01 => Main */
 public class TP01 {
-
-    public static int menu(){
-        int resp = -1;
-        Scanner sc = new Scanner(System.in);
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        System.out.println("CRUD - TP01");
-        System.out.println("Escolha Uma das Opcoes");
-        System.out.println("1 - Create");
-        System.out.println("2 - Update");
-        System.out.println("3 - Delete");
-        System.out.println("4 - Read");
-        System.out.println("0 - Sair");
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        try{
+            // arquivo CSV (base de dados)
+            String baseFile = "TP01/data/spotify.csv";
+            BufferedReader fr = new BufferedReader(new FileReader(baseFile));
+            
+            // arquivo RAF (registros em bytes) 
+            CRUD arquivo = new CRUD("musicas");
 
-        System.out.print("-> ");
-        resp =  sc.nextInt();
+            String line; // linha do CSV
+            int id = 1; // id do registro
+            
+            // lê 10 musicas (linhas) do CSV, faz parse e cria registro no arquivo
+            for(int i = 0; i < 10; i++){
+                line = fr.readLine();
+                
+                Musica musica = new Musica();
+                musica.parseCSV(line);
+                musica.setID(id);
 
-        sc.close();
-        return resp;
-    }
+                arquivo.create(musica); 
 
-    public static Musica lerMusica(int id)throws Exception{
-        Scanner sc = new Scanner(System.in);
-        Musica msc = new Musica();
-
-        msc.setID(id);
-
-        System.out.println("Digite o duration_ms da musica");
-        int duration_ms = Integer.parseInt(sc.nextLine());
-        msc.setDuration_ms(duration_ms);
-
-        System.out.println("Digite o release_date da musica [yyyy-MM-dd]");
-        Date release_date;
-        String StringDate = sc.nextLine();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");      
-        release_date = sdf.parse(StringDate);
-        msc.setRelease_date(release_date);
-
-        System.out.println("Digite o trackID da musica");
-        String track_id = sc.nextLine();
-        msc.setTrack_id(track_id);
-
-        System.out.println("Digite o trackID da musica");
-        String name = sc.nextLine();
-        msc.setName(name);
-
-        System.out.println("Digite o numero de artistas da musica");
-        ArrayList<String> artistas = new ArrayList<String>();
-        int num_artists = Integer.parseInt(sc.nextLine()); 
-        for(int i = 0; i < num_artists; i++){
-            artistas.add(sc.nextLine());
-        }
-        msc.setArtists(artistas);
-
-        sc.close();
-        return msc;
-    }
-
-    public static void main(String[] args)throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int opc = -1;
-        int id = 0;
-
-        CRUD arqivo = new CRUD("Teste");
-
-        String baseFile = ".../data/spotfy.csv";
-        FileInputStream fstream = new FileInputStream(baseFile);
-        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-
-        String line;
-
-        for(int i = 0; i < 10; i++){
-            line = br.readLine();
-            Musica musica = new Musica();
-            musica.readCSV(line);
-            musica.setID(id);
-            arqivo.create(musica);
-            id++;
-        }
-        
-        do{
-            opc = menu();
-            switch(opc){
-                case 0:
-                    arqivo.close();
-                    break;
-                case 1:
-                    Musica msc = lerMusica(id);
-                    arqivo.create(msc);
-                    id++;
-                    break;
-                case 2:
-                    System.out.println("Qual o id da musica que deve ser alterado ");
-                    int altera = Integer.parseInt(sc.nextLine());
-                    arqivo.update(altera);
-                    break;
-                case 3:
-                    System.out.println("Qual o id da musica que deve ser deletado ");   
-                    int deleta = Integer.parseInt(sc.nextLine());     
-                    arqivo.delete(deleta);
-                    break;
-                case 4:
-                    System.out.println("Qual o id que deseja ser lido ");
-                    int ler = Integer.parseInt(sc.nextLine());
-                    Musica read = arqivo.read(ler);
-                    read.toString();
-                    break;
-                default:
-                    System.out.println("Valor Invalido ");   
-
+                id++;
             }
 
-        }while(opc != 0);
+            int opc = -1; // opcao do menu
 
-        br.close();
-        sc.close();
+            do{
+                opc = menu();
+
+                // executa tarefas do menu de CRUD
+                switch(opc){ 
+                    case 0: { // fecha arquivo e encerra programa
+                        System.out.println("\n**Encerrando programa**");
+                        arquivo.close();
+                        break;
+                    }
+                    case 1: { // Create: lê atributos da musica e cria registro no arquivo
+                        System.out.println("\n**Criando musica**");
+                        
+                        Musica msc = lerMusica(id);
+                        System.out.println("\n" + msc);
+                        arquivo.create(msc);
+                                                
+                        break;
+                    } case 2: { // Read: lê ID da música, lê musica do arquivo e imprime
+                        System.out.println("\n**Lendo musica**");
+                        System.out.print("ID da musica a ser lida: ");
+                        
+                        int readID = Integer.parseInt(br.readLine());
+                        Musica msc = arquivo.read(readID);
+                        System.out.println("\n" + msc);
+                        
+                        break;
+                    } case 3: { // Update
+                        System.out.println("\n**Atualizando musica**");
+                        System.out.print("ID da musica que deve ser alterada: ");
+                        
+                        // TODO: implementar update
+                        // int updateID = Integer.parseInt(br.readLine());
+                        // if(arquivo.update(updateID))
+                        //     System.out.println("Musica atualizada com sucesso");
+                        // else
+                        //     System.out.println("Erro ao atualizar musica");
+                        
+                        break;
+                    } case 4: { // Delete: lê ID da música, procura no arquivo e exclui
+                        System.out.println("\n**Deletando musica**");
+                        System.out.print("ID da musica que deve ser deletada: ");   
+                        
+                        int deleteID = Integer.parseInt(br.readLine());     
+                        
+                        if(arquivo.delete(deleteID))
+                            System.out.println("Musica removida com sucesso");
+                        else
+                            System.out.println("Erro ao remover musica");
+                                                    
+                        break;
+                    }
+                }
+            } while(opc != 0);
+          
+            fr.close();
+            br.close();
+        } catch(FileNotFoundException fnfe){
+            System.err.println("Arquivo CSV da base de dados nao encontrado");
+            fnfe.printStackTrace();
+        } catch(IOException ioe){
+            System.err.println("Erro de leitura na funcao principal");
+            ioe.printStackTrace();
+        }
+    }
+    /**
+     * Mostra menu na tela e solicita ao usuário qual opção ele deseja executar (CRUD)
+     * @return int opção lida
+     */
+    public static int menu() {
+        System.out.println("\nCRUD - TP01");
+        System.out.println("Escolha uma das opcoes:");
+        System.out.println("0 - Sair");
+        System.out.println("1 - Create");
+        System.out.println("2 - Read");
+        System.out.println("3 - Update");
+        System.out.println("4 - Delete");
+        
+        int opc = -1;
+        boolean invalido = false;
+     
+        try{
+            do{
+                System.out.print("-> ");
+                opc = Integer.parseInt(br.readLine());
+                invalido = (opc < 0) || (opc > 4);
+                if(invalido) System.out.println("Opcao invalida! Digite novamente");
+            } while(invalido);
+        } catch(IOException ioe){
+            System.err.println("Erro ao ler opcao do menu");
+            ioe.printStackTrace();
+        }
+
+        return opc;
+    }
+    /**
+     * Solicita ao usuário que digite os atributos da musica, criando uma instância
+     * e retorna o objeto criado
+     * @param id identificador da Musica (int)
+     * @return objeto da musica lida
+     */
+    public static Musica lerMusica(int id) {   
+        int duration_ms = -1;
+        String track_id = "", name = "";
+        Date release_date = new Date();
+        ArrayList<String> artists = new ArrayList<String>();
+
+        try{
+            // Lê duration_ms
+            System.out.print("Duration_ms: ");
+            duration_ms = Integer.parseInt(br.readLine());
+
+            // Lê release_date
+            System.out.print("Release_date [yyyy-MM-dd]: ");
+            String stringDate = br.readLine();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");      
+            try{
+                release_date = sdf.parse(stringDate);
+            } catch(ParseException pe){
+                System.err.println("Erro ao fazer parse da data");
+                pe.printStackTrace();
+            }
+
+            // Lê track_id
+            System.out.print("Track_id: ");
+            track_id = br.readLine();
+
+            // Lê name
+            System.out.print("Name: ");
+            name = br.readLine();
+
+            // Lê artists
+            System.out.println("Artists [FIM quando terminar]:");
+            String line = br.readLine();
+            while( !(line.equals("FIM")) ){
+                artists.add(line);
+                line = br.readLine();
+            }
+        } catch(IOException ioe){
+            System.err.println("Erro ao ler atributo da musica");
+            ioe.printStackTrace();
+        }
+        
+        Musica msc = new Musica(id, duration_ms, release_date, track_id, name, artists);
+
+        return msc;
     }
 }
